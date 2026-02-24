775a22af6c338d877957d268a144de1b4618452e
Aleš Zima <zima.ales@seznam.cz>
2026-02-24 18:47:20 +0100

    added create fail test

--- src/routes/sets/sets.test.ts
+++ src/routes/sets/sets.test.ts
@@ -1,4 +1,4 @@
-import { beforeAll, describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import setsRouter from './sets.index.js'
 import createApp from '@/utils/create_app.js'
 import config from '@/config.js'
@@ -7,9 +7,10 @@ describe('Sets list', () => {
   it('successfully returns an array of sets', async () => {
     const testRouter = createApp(config).route('/', setsRouter)
 
-    const sets = await testRouter.request('/api/v1/sets')
-    const result = await sets.json()
+    const response = await testRouter.request('/api/v1/sets')
+    const result = await response.json()
 
+    expect(response.status).toBe(200)
     expect(result[0]).toEqual({
       id: 1,
       name: 'Lorwyn Eclipsed',
@@ -24,17 +25,19 @@ describe('Set by code', () => {
   it('returns 404 when set not found', async () => {
     const testRouter = createApp(config).route('/', setsRouter)
 
-    const set = await testRouter.request('/api/v1/sets/abc')
-    const result = await set.json()
+    const response = await testRouter.request('/api/v1/sets/abc')
+    const result = await response.json()
 
+    expect(response.status).toBe(404)
     expect(result).toEqual({ message: 'Set not found!' })
   })
   it('successfully returns a set', async () => {
     const testRouter = createApp(config).route('/', setsRouter)
 
-    const set = await testRouter.request('/api/v1/sets/ecl')
-    const result = await set.json()
+    const response = await testRouter.request('/api/v1/sets/ecl')
+    const result = await response.json()
 
+    expect(response.status).toBe(200)
     expect(result).toEqual({
       id: 1,
       name: 'Lorwyn Eclipsed',
@@ -42,5 +45,26 @@ describe('Set by code', () => {
       cardCount: 274,
       mechanics: ['Blight', 'Vivid', 'Behold', 'Changeling', 'Convoke', 'Evoke'],
     })
+  })
+})
+
+describe('Create set', () => {
+  it('fails to create set with existing code', async () => {
+    const testRouter = createApp(config).route('/', setsRouter)
+    const setData = {
+      name: 'Test set',
+      code: 'TS',
+      cardCount: 3,
+      mechanics: ['Awesomeness', 'Testing'],
+    }
+
+    const response = await testRouter.request('/api/v1/sets', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(setData),
+    })
+    const result = await response.json()
+
+    expect(response.status).toBe(500)
   })
 })
